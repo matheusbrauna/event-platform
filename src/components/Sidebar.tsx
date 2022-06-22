74c6ae31a1ff2f8982dd7445ee3324800ab6c3ec
matheus henrique <mattsilvahenrique102@gmail.com>
2022-06-22 13:53:04 -0300

    aula02

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,48 @@
+import { gql, useQuery } from '@apollo/client';
+import { Lesson } from './Lesson';
+
+const GET_LESSON_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`;
+
+interface GetLessonQueryResponse {
+  lessons: {
+    id: string;
+    lessonType: 'live' | 'class';
+    availableAt: string;
+    title: string;
+    slug: string;
+  }[];
+}
+
+export function Sidebar() {
+  const { data } = useQuery<GetLessonQueryResponse>(GET_LESSON_QUERY);
+
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border rounded-b mr-5 border-gray-600 h-[650px] overflow-auto">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de aulas
+      </span>
+
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map(lesson => (
+          <Lesson
+            key={lesson.id}
+            title={lesson.title}
+            slug={lesson.slug}
+            availableAt={new Date(lesson.availableAt)}
+            type={lesson.lessonType}
+          />
+        ))}
+      </div>
+    </aside>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
+      colors: {
+        primary: {
+          300: 'hsl(var(--green-300) / 100)',
+          500: 'hsl(var(--green-500) / 100)',
+          700: 'hsl(var(--green-700) / 100)',
+        },
+        blue: {
+          500: 'hsl(var(--blue-500) / 100)',
+        },
+        orange: {
+          500: 'hsl(var(--orange-500) / 100)',
+        },
+        red: {
+          500: 'hsl(var(--red-500) / 100)',
+        },
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          500: '#323238',
+          600: '#29292E',
+          700: '#121214',
+          900: '#09090A',
+        },
+      },
+    },
   },
   plugins: [],
 };
