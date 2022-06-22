5fa2371de89e9c00876e5b1a58e02f35a46df954
matheus henrique <mattsilvahenrique102@gmail.com>
2022-06-22 17:37:19 -0300

    scroll feature

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -27,7 +27,7 @@ export function Sidebar() {
   const { data } = useQuery<GetLessonQueryResponse>(GET_LESSON_QUERY);
 
   return (
-    <aside className="w-[348px] bg-gray-700 p-6 border rounded-b mr-5 border-gray-600 h-[650px] overflow-auto">
+    <aside className="max-w-[348px] bg-gray-700 p-6 border rounded-b mr-5 border-gray-600 max-h-[650px] overflow-auto scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-transparent">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
